c9033efa3cc22bb5f014a00f3d1948093d465b72
Abhijeet-Bhushari <abhijeetvijaybhushari242002@gmail.com>
2023-10-12 18:07:44 +0530

    12-10-23

--- Find In Mountain Array/solution.cpp
+++ Find In Mountain Array/solution.cpp
@@ -11,33 +11,41 @@
 class Solution {
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        int l = 0, r = mountainArr.length() - 1;
+        int len = mountainArr.length();
+        int l = 0, r = len - 1;
         int peak = 0;
         while(l <= r){
             int m = (l + r) / 2;
-            if( ( m-1 >= 0 && mountainArr.get(m) > mountainArr.get(m - 1) ) && ( m + 1 < mountainArr.length() && mountainArr.get(m) > mountainArr.get(m + 1) ) ){
+            int at_m = mountainArr.get(m), at_bm, at_am;
+            if(m > 0) at_bm = mountainArr.get(m - 1);
+            if(m < len - 1) at_am = mountainArr.get(m + 1);
+            
+            if( ( m-1 >= 0 && at_m > at_bm ) && ( m + 1 < len && at_m > at_am ) ){
                 peak = m;
                 break;
             }
-            else if(m - 1 >= 0 && mountainArr.get(m - 1) < mountainArr.get(m)) l = m + 1;
+            else if(m - 1 >= 0 && at_bm < at_m) l = m + 1;
             else if(m == 0) l = m + 1;
             else r = m - 1;
         }
+        cout<<"peak: "<<peak<<endl;
         if(target > mountainArr.get(peak)) return -1;
         l = 0; 
         r = peak;
         while(l<=r){
             int m = (l + r) / 2;
-            if(mountainArr.get(m) == target) return m;
-            else if(mountainArr.get(m) < target) l = m + 1;
+            int at_m = mountainArr.get(m);
+            if(at_m == target) return m;
+            else if(at_m < target) l = m + 1;
             else r = m - 1;
         }
         l = peak; 
-        r = mountainArr.length() - 1;
+        r = len - 1;
         while(l<=r){
             int m = (l + r) / 2;
-            if(mountainArr.get(m) == target) return m;
-            else if(mountainArr.get(m) < target) r = m - 1;
+            int at_m = mountainArr.get(m);
+            if(at_m == target) return m;
+            else if(at_m < target) r = m - 1;
             else l = m + 1;
         }
         return -1;

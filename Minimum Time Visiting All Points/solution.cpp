4a740a4e6c3ebc332ed0eba66d144bec2b2d5e56
Abhijeet-Bhushari <115354940+Abhijeet-Bhushari@users.noreply.github.com>
2023-12-03 23:12:33 +0530

    03-12-23

---
+++ Minimum Time Visiting All Points/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = 0;
+        for(int i=0; i<n-1; i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
+            ans += max(abs(y2 - y1), abs(x2 - x1));
+        }
+        return ans;
+    }
+};
